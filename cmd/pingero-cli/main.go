0b1918087212d43dd09c4e61d616d4ac17e0312b
Pedro Williams <pedrowilliamsbc@gmail.com>
2025-06-01 22:35:13 -0300

    refactor: modify main.go

--- cmd/pingero-cli/main.go
+++ cmd/pingero-cli/main.go
@@ -1,131 +1,16 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"io"
-	"net/http"
 	"os"
-	"strconv"
-	"strings"
-	"time"
-)
 
-type Command int
-
-const (
-	CmdStart Command = 1
-	CmdView  Command = 2
-	CmdStop  Command = 3
+	"github.com/pedrowilliamss/pingero-cli/internal"
 )
 
 func main() {
-	exibeIntroducao()
-
-	for {
-		exibeMenu()
-
-		switch leComando() {
-		case CmdStart:
-			iniciarMonitoramento()
-		case CmdView:
-			fmt.Println("Exibindo logs...")
-			imprimeLogs()
-		case CmdStop:
-			fmt.Println("Saindo do programa")
-			os.Exit(0)
-		default:
-			fmt.Println("Não conheço este comando")
-			os.Exit(-1)
-		}
-	}
-}
-
-func exibeIntroducao() {
-	nome := "Pedro"
-	versao := 1.1
-	fmt.Println("Olá, sr.", nome)
-	fmt.Println("Este programa está na versão", versao)
-}
-
-func exibeMenu() {
-	fmt.Println("1- Iniciar Monitoramento")
-	fmt.Println("2- Exibir logs")
-	fmt.Println("0- Sair do Programa")
-}
-
-func leComando() Command {
-	var command int
-	fmt.Scan(&command)
-	fmt.Println("")
-
-	return Command(command)
-}
-
-func iniciarMonitoramento() {
-	fmt.Println("Monitorando...")
-
-	sites := leSitesDoArquivo()
-
-	for {
-		for _, site := range sites {
-			testaSite(site)
-		}
-		time.Sleep(time.Second * 5)
-	}
-}
-
-func testaSite(url string) {
-	fmt.Println("Testando site: ", url)
-	res, err := http.Get(url)
-	if err != nil {
-		fmt.Println("Ocorreu um erro:", err)
+	cli := internal.CLI{
+		MessagePublisher: os.Stdout,
+		MessageConsumer:  os.Stdin,
 	}
 
-	if res.StatusCode == http.StatusOK {
-		fmt.Println("Site:", url, "foi carregado com sucesso!")
-		registraLog(url, true)
-	} else {
-		fmt.Println("Site:", url, "esta com problemas. Status Code:", res.StatusCode)
-		registraLog(url, false)
-	}
-}
-
-func leSitesDoArquivo() []string {
-	arquivo, err := os.Open("sites.txt")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer arquivo.Close()
-
-	sites := make([]string, 0, 10)
-	leitor := bufio.NewReader(arquivo)
-	for {
-		linha, err := leitor.ReadString('\n')
-		if err == io.EOF {
-			break
-		}
-		sites = append(sites, strings.TrimSpace(linha))
-	}
-
-	return sites
-}
-
-func registraLog(site string, status bool) {
-	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer arquivo.Close()
-
-	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + "- online: " + strconv.FormatBool(status) + "\n")
-}
-
-func imprimeLogs() {
-	arquivo, err := os.ReadFile("log.txt")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	fmt.Println(string(arquivo))
+	cli.Start()
 }
